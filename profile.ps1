27f738d39015428d4406ab26908e2b21075d46b9
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-08-15 15:29:19 +0100

    Alias cd to change directory to home

--- profile.ps1
+++ profile.ps1
@@ -1,9 +1,24 @@
-if ($host.Name -eq 'ConsoleHost')
-{
+if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadLine
 
     Set-PSReadlineOption -EditMode vi
     Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
     Import-Module PSFzf
+
+    # Alias `cd` to change directory to home when no argument is passed.
+    function ChangeDirectory {
+        param(
+            [parameter(Mandatory = $false)]
+            $path
+        )
+        if ( $PSBoundParameters.ContainsKey('path') ) {
+            Set-Location $path
+        }
+        else {
+            Set-Location $home
+        }
+    }
+    Remove-Item alias:\cd
+    New-Alias cd ChangeDirectory
 }

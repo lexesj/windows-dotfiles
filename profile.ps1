7c0494540ecf4823f343dfbfb5f65c892eb1cc4c
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-12 23:02:23 +0100

    Return early instead of indentation

--- profile.ps1
+++ profile.ps1
@@ -13,152 +13,154 @@
 ### This is the default policy on Windows Server 2012 R2 and above for server Windows. For
 ### more information about execution policies, run Get-Help about_Execution_Policies.
 
-if ($host.Name -eq 'ConsoleHost')
+if ($host.Name -ne 'ConsoleHost')
 {
-    # Early imports
-    Import-Module posh-git
+    return
+}
 
-    # Find out if the current user identity is elevated (has admin rights)
-    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-    $principal = New-Object Security.Principal.WindowsPrincipal $identity
-    $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+# Early imports
+Import-Module posh-git
 
-    # Useful shortcuts for traversing directories
-    function cd...
-    { Set-Location ..\.. 
-    }
-    function cd....
-    { Set-Location ..\..\.. 
-    }
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-    # Compute file hashes - useful for checking successful downloads
-    function md5
-    { Get-FileHash -Algorithm MD5 $args 
-    }
-    function sha1
-    { Get-FileHash -Algorithm SHA1 $args 
-    }
-    function sha256
-    { Get-FileHash -Algorithm SHA256 $args 
-    }
+# Useful shortcuts for traversing directories
+function cd...
+{ Set-Location ..\.. 
+}
+function cd....
+{ Set-Location ..\..\.. 
+}
 
-    # Quick shortcut to start notepad
-    function n
-    { notepad $args 
-    }
+# Compute file hashes - useful for checking successful downloads
+function md5
+{ Get-FileHash -Algorithm MD5 $args 
+}
+function sha1
+{ Get-FileHash -Algorithm SHA1 $args 
+}
+function sha256
+{ Get-FileHash -Algorithm SHA256 $args 
+}
 
-    # Drive shortcuts
-    function HKLM:
-    { Set-Location HKLM: 
-    }
-    function HKCU:
-    { Set-Location HKCU: 
-    }
-    function Env:
-    { Set-Location Env: 
-    }
+# Quick shortcut to start notepad
+function n
+{ notepad $args 
+}
 
-    # Creates drive shortcut for OneDrive, if current user account is using it
-    if (Test-Path HKCU:\SOFTWARE\Microsoft\OneDrive)
+# Drive shortcuts
+function HKLM:
+{ Set-Location HKLM: 
+}
+function HKCU:
+{ Set-Location HKCU: 
+}
+function Env:
+{ Set-Location Env: 
+}
+
+# Creates drive shortcut for OneDrive, if current user account is using it
+if (Test-Path HKCU:\SOFTWARE\Microsoft\OneDrive)
+{
+    $onedrive = Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\OneDrive
+    if ($onedrive.UserFolder -And (Test-Path $onedrive.UserFolder))
     {
-        $onedrive = Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\OneDrive
-        if ($onedrive.UserFolder -And (Test-Path $onedrive.UserFolder))
-        {
-            New-PSDrive -Name OneDrive -PSProvider FileSystem -Root $onedrive.UserFolder -Description "OneDrive"
-            function OneDrive:
-            { Set-Location OneDrive: 
-            }
+        New-PSDrive -Name OneDrive -PSProvider FileSystem -Root $onedrive.UserFolder -Description "OneDrive"
+        function OneDrive:
+        { Set-Location OneDrive: 
         }
-        Remove-Variable onedrive
     }
+    Remove-Variable onedrive
+}
 
-    # Set up command prompt and window title. Use UNIX-style convention for identifying
-    # whether user is elevated (root) or not. Window title shows current version of PowerShell
-    # and appends [ADMIN] if appropriate for easy taskbar identification
-    $GitPromptSettings.BeforePath = "["
-    $GitPromptSettings.AfterPath = "]"
-    if ($isAdmin)
+# Set up command prompt and window title. Use UNIX-style convention for identifying
+# whether user is elevated (root) or not. Window title shows current version of PowerShell
+# and appends [ADMIN] if appropriate for easy taskbar identification
+$GitPromptSettings.BeforePath = "["
+$GitPromptSettings.AfterPath = "]"
+if ($isAdmin)
+{
+    $GitPromptSettings.DefaultPromptSuffix = " # "
+} else
+{
+    $GitPromptSettings.DefaultPromptSuffix = " $ "
+}
+
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin)
+{
+    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs
+{
+    if ($args.Count -gt 0)
     {
-        $GitPromptSettings.DefaultPromptSuffix = " # "
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
     } else
     {
-        $GitPromptSettings.DefaultPromptSuffix = " $ "
+        Get-ChildItem -Recurse | Foreach-Object FullName
     }
-
-    $Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
-    if ($isAdmin)
-    {
-        $Host.UI.RawUI.WindowTitle += " [ADMIN]"
-    }
-
-    # Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
-    function dirs
-    {
-        if ($args.Count -gt 0)
-        {
-            Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
-        } else
-        {
-            Get-ChildItem -Recurse | Foreach-Object FullName
-        }
-    }
-
-    # Simple function to start a new elevated process. If arguments are supplied then
-    # a single command is started with admin rights; if not then a new admin instance
-    # of PowerShell is started.
-    function admin
-    {
-        if ($args.Count -gt 0)
-        {
-            $argList = "& '" + $args + "'"
-            Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
-        } else
-        {
-            Start-Process "$psHome\pwsh.exe" -Verb runAs
-        }
-    }
-
-    # Set UNIX-like aliases for the admin command, so sudo <command> will run the command
-    # with elevated rights.
-    Set-Alias -Name su -Value admin
-    Set-Alias -Name sudo -Value admin
-
-    # Make it easy to edit this profile once it's installed
-    function Edit-Profile
-    {
-        nvim $profile.CurrentUserAllHosts
-    }
-
-    # We don't need these any more; they were just temporary variables to get to $isAdmin.
-    # Delete them to prevent cluttering up the user profile.
-    Remove-Variable identity
-    Remove-Variable principal
-
-    # Vi mode
-    Set-PSReadlineOption -EditMode vi
-    Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-
-    # Imports
-    Import-Module PSReadLine
-    Import-Module PSFzf
-
-    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-    # Fast Node Manager.
-    if (Get-Command "fnm" -errorAction SilentlyContinue)
-    {
-        fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-    }
-
-    # Zoxide.
-    if (Get-Command "zoxide" -errorAction SilentlyContinue)
-    {
-        Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
-    }
-
-    # Neovim as main editor.
-    $env:EDITOR = "nvim"
-
-    # Set default shell as PowerShell.
-    $env:SHELL = "pwsh"
 }
+
+# Simple function to start a new elevated process. If arguments are supplied then
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin
+{
+    if ($args.Count -gt 0)
+    {
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
+    } else
+    {
+        Start-Process "$psHome\pwsh.exe" -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights.
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile
+{
+    nvim $profile.CurrentUserAllHosts
+}
+
+# We don't need these any more; they were just temporary variables to get to $isAdmin.
+# Delete them to prevent cluttering up the user profile.
+Remove-Variable identity
+Remove-Variable principal
+
+# Vi mode
+Set-PSReadlineOption -EditMode vi
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+# Imports
+Import-Module PSReadLine
+Import-Module PSFzf
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Fast Node Manager.
+if (Get-Command "fnm" -errorAction SilentlyContinue)
+{
+    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+}
+
+# Zoxide.
+if (Get-Command "zoxide" -errorAction SilentlyContinue)
+{
+    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+}
+
+# Neovim as main editor.
+$env:EDITOR = "nvim"
+
+# Set default shell as PowerShell.
+$env:SHELL = "pwsh"

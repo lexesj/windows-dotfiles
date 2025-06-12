821a2ce7f4f3032690b2603d543babebfc7580e4
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-12 22:49:51 +0100

    Fix admin shortcut

--- profile.ps1
+++ profile.ps1
@@ -82,10 +82,10 @@ if ($host.Name -eq 'ConsoleHost') {
     function admin {
         if ($args.Count -gt 0) {
             $argList = "& '" + $args + "'"
-            Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+            Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
         }
         else {
-            Start-Process "$psHome\powershell.exe" -Verb runAs
+            Start-Process "$psHome\pwsh.exe" -Verb runAs
         }
     }
 

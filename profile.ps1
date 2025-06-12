638110c0a0b6555f37bce39326d101d914a8a7bf
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-12 22:47:42 +0100

    Replace home directory shortcut with zoxide in-built

--- profile.ps1
+++ profile.ps1
@@ -114,22 +114,6 @@ if ($host.Name -eq 'ConsoleHost') {
     Set-PSReadlineOption -EditMode vi
     Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
-    # Alias `cd` to change directory to home when no argument is passed.
-    function ChangeDirectory {
-        param(
-            [parameter(Mandatory = $false)]
-            $path
-        )
-        if ( $PSBoundParameters.ContainsKey('path') ) {
-            Set-Location $path
-        }
-        else {
-            Set-Location $home
-        }
-    }
-    Remove-Item alias:\cd
-    New-Alias cd ChangeDirectory
-
     # Imports
     Import-Module PSReadLine
     Import-Module PSFzf

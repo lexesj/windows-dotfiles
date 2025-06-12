32cd837ae2c3cbabcf86e8462c5d806def5658a0
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-12 23:22:22 +0100

    Remove admin function with arguments

--- profile.ps1
+++ profile.ps1
@@ -111,14 +111,7 @@ function dirs
 # of PowerShell is started.
 function admin
 {
-    if ($args.Count -gt 0)
-    {
-        $argList = "& '" + $args + "'"
-        Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
-    } else
-    {
-        Start-Process "$psHome\pwsh.exe" -Verb runAs
-    }
+    Start-Process wt -Verb runAs -ArgumentList "-- pwsh"
 }
 
 # Set UNIX-like aliases for the admin command, so sudo <command> will run the command

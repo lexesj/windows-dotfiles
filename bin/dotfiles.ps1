f544de80da84c0a1a3103f433325ab6c44d301ad
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-14 11:18:48 +0100

    Change tabs for spaces

--- bin/dotfiles.ps1
+++ bin/dotfiles.ps1
@@ -2,42 +2,42 @@ $DOTFILES_DIR = "$HOME\.windows-dotfiles"
 
 function Test-IsAdmin
 {
-	$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-	$principal = New-Object Security.Principal.WindowsPrincipal $identity
-	$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal $identity
+    $isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-	return $isAdmin
+    return $isAdmin
 }
 
 function Install-Dependencies
 {
-	if (!(Get-Command git -errorAction SilentlyContinue))
-	{
-		Write-Host "Installing git..."
-		winget install Git.Git
-	}
+    if (!(Get-Command git -errorAction SilentlyContinue))
+    {
+        Write-Host "Installing git..."
+        winget install Git.Git
+    }
 
-	if (!(Get-Command pwsh -errorAction SilentlyContinue))
-	{
-		Write-Host "Installing PowerShell..."
-		winget install Microsoft.PowerShell
-	}
+    if (!(Get-Command pwsh -errorAction SilentlyContinue))
+    {
+        Write-Host "Installing PowerShell..."
+        winget install Microsoft.PowerShell
+    }
 }
 
 if (!(Test-IsAdmin))
 {
-	Write-Error "Please re-run this script as an administrator."
-	exit 1
+    Write-Error "Please re-run this script as an administrator."
+    exit 1
 }
 
 Install-Dependencies
 
 if (!(Test-Path -Path $DOTFILES_DIR))
 {
-	Write-Host "Cloning dotfiles repository..."
-	git clone --quiet https://github.com/lexesj/windows-dotfiles.git "$DOTFILES_DIR"
+    Write-Host "Cloning dotfiles repository..."
+    git clone --quiet https://github.com/lexesj/windows-dotfiles.git "$DOTFILES_DIR"
 } else
 {
-	Write-Host "Updating dotfiles repository..."
-	git -C "$DOTFILES_DIR" pull --quiet
+    Write-Host "Updating dotfiles repository..."
+    git -C "$DOTFILES_DIR" pull --quiet
 }

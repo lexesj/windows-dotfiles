1519c54f67f919588f83447655f04bf41c411a34
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-04-05 18:44:03 +0100

    feat!: remove unix-dotfiles dependency

--- bin/Dotfiles.psm1
+++ bin/Dotfiles.psm1
@@ -5,16 +5,10 @@ enum Tag
     Winget
     Git
     PowerShell
-    Vim
     Terminal
     PowerToys
     CliTools
     SshKey
-}
-
-function Test-SubmoduleAuthenticated
-{
-    return git -C "$env:DOTFILES_PATH\unix-dotfiles" remote -v | Select-String -Pattern "git@github.com" -Quiet
 }
 
 function Update-Dotfiles
@@ -23,27 +17,6 @@ function Update-Dotfiles
     (
         [Tag[]]$Tags = [Tag].GetEnumValues()
     )
-
-    if (!(Test-Path -Path $env:DOTFILES_PATH))
-    {
-        Write-Host "Cloning dotfiles repository..."
-        git clone --quiet --recurse-submodules https://github.com/lexesj/windows-dotfiles.git $env:DOTFILES_PATH
-    } else
-    {
-        Write-Host "Updating dotfiles repository..."
-        git -C $env:DOTFILES_PATH pull --quiet
-        git -C $env:DOTFILES_PATH submodule update --remote
-        git -C $env:DOTFILES_PATH diff --quiet "$env:DOTFILES_PATH\unix-dotfiles"
-
-        $HasChanges = !$?
-        if (Test-SubmoduleAuthenticated -and $HasChanges)
-        {
-            Write-Host "Updating unix-dotfiles submodule..."
-            git -C $env:DOTFILES_PATH add "$env:DOTFILES_PATH\unix-dotfiles"
-            git -C $env:DOTFILES_PATH commit --quiet -m "Update unix-dotfiles submodule"
-            git -C $env:DOTFILES_PATH push --quiet
-        }
-    }
 
     Import-Module "$env:DOTFILES_PATH\bin\Installers.psm1" -Force
 
